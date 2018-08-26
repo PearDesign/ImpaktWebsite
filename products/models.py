21cd4644e220f5dfb4956914b46f9fe6417164fa
Robert Townley <Me@RobertTownley.com>
2018-08-26 11:16:58 -0400

    Installing django taggit

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Seller(models.Model):
+    '''A company selling products on Amazon'''
+    ASIN = models.CharField(max_length=128, db_index=True, unique=True)
+    tags = TaggableManager()
+
+
+class Product(models.Model):
+    '''Representation of a project on Amazon'''
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=512, db_index=True, unique=True)
+    tags = TaggableManager()
